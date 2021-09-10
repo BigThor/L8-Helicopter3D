a1e89bb5ecc231c382f3bf472af941c1bfb072b9
Víctor Adrián Suarez Ruiz <jr._5oct1998@hotmail.com>
2021-09-10 12:56:42 -0500

    Add gem spawns

---
+++ Assets/Resources/Scripts/GemSpawner.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GemSpawner : MonoBehaviour {
+
+	public GameObject[] prefabs;
+
+	// Use this for initialization
+	void Start () {
+
+		// infinite coin spawning function, asynchronous
+		StartCoroutine(SpawnGems());
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	IEnumerator SpawnGems() {
+		while (true) {
+			Debug.Log("hi");
+			// instantiate gem
+			Instantiate(prefabs[Random.Range(0, prefabs.Length)], new Vector3(26, Random.Range(-10, 10), 10), Quaternion.identity);
+
+			// pause until the next spawns
+			yield return new WaitForSeconds(Random.Range(7, 10));
+		}
+	}
+}
